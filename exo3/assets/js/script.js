fee45904665fe4d796b2fb582e09a24620ef6ed5
StephaneC <stephane.cavillon@itech.fr>
2020-11-16 21:08:34 +0100

    fin exo3 sans enjolivure

--- exo3/assets/js/script.js
+++ exo3/assets/js/script.js
@@ -4,10 +4,20 @@ let p = document.querySelectorAll('p');
 // reste à creer les listeners avec le forEach
 //d'abord poser la class 'invisible' sur tout les <p> à la fin du chargement de la page
 window.addEventListener('load', function (){
-    console.log('load !');
     p.forEach(item => {item.classList.add('invisible')
     });
 });
 
 
 //ensuite remplacer par 'visible'
+p.forEach(item => {
+    window.addEventListener('scroll', function (){
+        if(scrollY > (item.offsetTop - 100)){
+            item.classList.replace('invisible', 'visible');
+        } else {
+            console.log('invisible !');
+            item.classList.replace('visible', 'invisible');
+        };
+    });
+});
+
